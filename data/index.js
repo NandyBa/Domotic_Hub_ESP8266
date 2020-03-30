1545e6a558ca2d6d360f4bc5d26fbc3676f2e0fa
Nandy Bâ <nandyba@yahoo.fr>
2020-03-30 02:21:00 +0200

    Add of ChangePhilipsHueState to limit code repetition

--- data/index.js
+++ data/index.js
@@ -1,9 +1,17 @@
+function ChangePhilipsHueState(data, light_id){
+	var json = JSON.stringify(data);
+	var xhr = new XMLHttpRequest();
+	var url = "http://"+globalConfig.PhilipsHubIp+"/api/"+globalConfig.PhilipsUsername+"/lights/"+light_id+"/state";
+	xhr.open("PUT", url, true);
+	xhr.send(json);
+	return xhr.status
+}
+
 cbx = document.getElementsByClassName("cbx")
 for(i=0;i<cbx.length;i++){
 	cbx[i].onclick = function() {
 		var data = {};
 		light_id = this.value;
-		var xhr = new XMLHttpRequest();
 		if(this.checked){
 			//On donne comme consigne d'allumer la lampe
 			data.on = true;
@@ -11,12 +19,7 @@ for(i=0;i<cbx.length;i++){
 			//On donne comme consigne d'éteindre la lampe
 			data.on = false;
 		}
-		var json = JSON.stringify(data);
-
-		var xhr = new XMLHttpRequest();
-		var url = "http://"+globalConfig.PhilipsHubIp+"/api/"+globalConfig.PhilipsUsername+"/lights/"+light_id+"/state";
-		xhr.open("PUT", url, true);
-		xhr.send(json);
+		ChangePhilipsHueState(data, light_id);
 	}
 }
 row_interface = document.getElementsByClassName("row-interface");
@@ -30,11 +33,7 @@ for (var i = row_interface.length - 1; i >= 0; i--) {
 		var data = {};
 		data.on = true;
 		data.bri  = parseInt(254* parseInt(val)/100);
-		var json = JSON.stringify(data);
 
-		var xhr = new XMLHttpRequest();
-		var url = "http://"+globalConfig.PhilipsHubIp+"/api/"+globalConfig.PhilipsUsername+"/lights/"+light_id+"/state";
-		xhr.open("PUT", url, true);
-		xhr.send(json);
+		ChangePhilipsHueState(data, light_id);
 	}
 }
